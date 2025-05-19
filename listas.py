76f3639c59e0ea8c10dda30775283e5fd0ece8e3
OlgaPio <lm4109080@gmail.com>
2025-05-19 02:53:52 -0500

    listas

---
+++ listas.py
@@ -0,0 +1,15 @@
+# Crear una lista con diferentes tipos de elementos
+lista = [10, "Python", 3.14, True, "Estudiante", 42, False, "Ciencia", 99, "Programación"]
+
+# Acceder y modificar elementos
+print("Lista original:", lista)
+lista[1] = "JavaScript"
+print("Lista modificada:", lista)
+
+# Agregar un nuevo elemento
+lista.append("Nuevo Elemento")
+print("Lista después de agregar:", lista)
+
+# Eliminar un elemento
+lista.remove(42)
+print("Lista después de eliminar:", lista)
