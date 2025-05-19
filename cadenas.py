af4b71346a11c39cd7003b4c2994eaca3d357f4a
OlgaPio <lm4109080@gmail.com>
2025-05-19 02:53:33 -0500

    manipulacion de cadenas

---
+++ cadenas.py
@@ -0,0 +1,10 @@
+# Captura de frases
+frase1 = input("Ingrese la frase 1: ")
+frase2 = input("Ingrese la frase 2: ")
+
+# Concatenación y uso de f-strings
+frase_completa = frase1 + " " + frase2
+print(f"Frase concatenada: {frase_completa}")
+
+# Otra forma usando f-strings
+print(f"Las frases ingresadas son: '{frase1}' y '{frase2}'.")
