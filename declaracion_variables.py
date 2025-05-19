f948c384945a7428e62347da5761596d154541fd
OlgaPio <lm4109080@gmail.com>
2025-05-19 02:52:43 -0500

    declaracion de variables

---
+++ declaracion_variables.py
@@ -0,0 +1,11 @@
+# Captura de variables por teclado
+nombre = input("Ingrese nombre: ")
+edad = int(input("Ingrese edad: "))
+altura = float(input("Ingrese altura en metros: "))
+es_estudiante = bool(int(input("¿Eres estudiante? (1 para Sí, 0 para No): ")))
+
+# Imprimir valores en la consola
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad}")
+print(f"Altura: {altura}")
+print(f"Es estudiante: {es_estudiante}")
